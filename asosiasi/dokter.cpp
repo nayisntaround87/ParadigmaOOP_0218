3801507c3afa55a072931c7d6f5d89a995730757
nayisntaround87 <naylasalwahayati@gmail.com>
2024-05-21 20:22:44 +0700

    menambah tanda "

--- asosiasi/dokter.cpp
+++ asosiasi/dokter.cpp
@@ -8,10 +8,10 @@ public:
     string nama;
     vector<dokter*> daftar_dokter;
     pasien(string pNama) :nama(pNama) {
-        cout << "Pasien \"" << nama << "\ ada\n";
+        cout << "Pasien \"" << nama << "\" ada\n";
     }
     ~pasien() {
-        cout << "Pasien \"" << nama << "\ tidak ada\n";
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
     }
     void tambahDokter(dokter*);
     void cetakDokter();
